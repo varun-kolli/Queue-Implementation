b191ca14853f1d1d4f93a98297ae7d0eb6948dd0
varun-kolli <vkolli@calpoly.edu>
2020-10-05 19:12:47 -0700

    new links

--- queue_linked.py
+++ queue_linked.py
@@ -1,18 +1,17 @@
-
 class Node:
-    def __init__(self, item):
+    def __init__(self,item):
         self.item = item
         self.next = None
 
 class Queue:
     '''Implements an link-based ,efficient first-in first-out Abstract Data Type'''
+
     def __init__(self, capacity):
         '''Creates an empty Queue with a capacity'''
-        self.front = None
-        self.rear = None
         self.capacity = capacity
+        self.rear = None
+        self.front = None
         self.count = 0
-
 
 
     def is_empty(self):
@@ -26,44 +25,37 @@ class Queue:
 
 
     def enqueue(self, item):
-        '''If Queue is not full, enqueues (adds) item to Queue 
-           If Queue is full when enqueue is attempted, raises IndexError'''
+        '''If Queue is not full, enqueues (adds) item to Queue
+                   If Queue is full when enqueue is attempted, raises IndexError'''
         if self.is_full():
             raise IndexError
-        else:
-            if not self.rear: #basically, if there's nothing there, the front and back are the same thing
-                self.front = Node(item)
-                self.rear = self.front
-            else:
-                self.rear.next = Node(item)
-                self.rear = Node(item)
+
+        if self.front == None:
+            self.front = Node(item)
+            self.rear = self.front
             self.count += 1
 
+        else:
+            node = Node(item)
+            self.rear.next = node
+            self.rear = node
+            self.count += 1
+
+
     def dequeue(self):
-            '''If Queue is not empty, dequeues (removes) item from Queue and returns item.
-               If Queue is empty when dequeue is attempted, raises IndexError'''
-            if self.is_empty():
-                raise IndexError
-            else:
-                self.count -= 1
-                if not self.front:
-                    return None
-                else:
-                    ret = self.front.item
-                    self.front = self.front.next
-                    return ret
+        '''If Queue is not empty, dequeues (removes) item from Queue and returns item.
+                 If Queue is empty when dequeue is attempted, raises IndexError'''
 
+        if self.is_empty():
+            raise IndexError
+        else:
+            node = self.front
+            self.front = node.next
+            self.count -= 1
 
+        return node.item
 
 
     def size(self):
-            '''Returns the number of elements currently in the Queue, not the capacity'''
-            return self.count
-
-q = Queue(3)
-q.enqueue(1)
-q.enqueue(2)
-q.enqueue(3)
-#q = [1, 2, 3]
-
-
+        '''Returns the number of elements currently in the Queue, not the capacity'''
+        return self.count
